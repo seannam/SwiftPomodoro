f304ca0b8454811e7ea59e8f20abc7f7bd8c8fcb
Sean Nam <namsangwoo1@gmail.com>
2017-01-14 00:36:12 -0800

    Initial commit

--- SwiftPomodoro/ViewController.swift
+++ SwiftPomodoro/ViewController.swift
@@ -9,10 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var secondsLabel: UILabel!
+    @IBOutlet var minutesLabel: UILabel!
+    
+    var minutes = 02
+    var seconds = 05;
+    var timer = Timer();
+    var timerIsOn = false;
+    
+    
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        if(timerIsOn == false) { // false means off, true means on
+            sender.setTitle("PAUSE", for: .normal);
+            startTimer()
+        }
+        else {
+            sender.setTitle("START", for: .normal);
+            stopTimer()
+        }
+    }
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        
+        stopTimer()
+        if(timerIsOn == false) {
+            minutes = 01;
+            seconds = 02;
+    
+            updateLabels(minutes, seconds)
+        }
+    }
+    
+    func updateLabels(minutes:int, seconds:int) {
+        minutesLabel.text = String(format: "%02d", minutes);
+        secondsLabel.text = String(format: "%02d", seconds);
+    }
+    
+    func updateTimer() {
+        if (seconds > 0 && minutes >= 0) {
+            seconds -= 1;
+        }
+        
+        if ( minutes > 0 && seconds == 0) {
+            minutes -= 1;
+            seconds = 59;
+        }
+        
+        if ( minutes == 0 && seconds == 0) {
+            minutes = 00;
+            seconds = 00;
+            stopTimer()
+        }
+        updateLabels(minutes, seconds)
+    }
 
+    func startTimer() {
+        // timeInterval - per sec, selector - function to call
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true);
+        timerIsOn = true;
+    }
+    
+    func stopTimer() {
+        timer.invalidate()
+        timerIsOn = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        minutesLabel.text = String(format: "%02d", minutes);
+        secondsLabel.text = String(format: "%02d", seconds);
     }
 
     override func didReceiveMemoryWarning() {
